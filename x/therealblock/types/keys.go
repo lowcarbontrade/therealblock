999aa06b2a58468c362398a3fe632daf3ab8d4d3
diegogonzalezfe <diego.gonzalezfe@alumnos.upm.es>
2023-02-22 15:47:25 +0100

    Changes format to cammel style

--- x/therealblock/types/keys.go
+++ x/therealblock/types/keys.go
@@ -20,7 +20,7 @@ const (
 	ProjectCountKey = "Project/count/"
 
 	// ProjectCreatedEvent is the event type for project creation
-	ProjectCreatedEventType    = "project_created"
+	ProjectCreatedEventType    = "ProjectCreated"
 
 	// General event attributes
 	ProjectEventProjectKey     = "id"
