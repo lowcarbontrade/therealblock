a2778c5a29e4c5a5b66e2fa69680730a8f3f0986
marcosmartinezfco <marcosmartinezfco@gmail.com>
2023-03-04 12:04:20 +0100

    Add signer address admin verification

--- x/therealblock/keeper/msg_server_change_state.go
+++ x/therealblock/keeper/msg_server_change_state.go
@@ -9,6 +9,9 @@ import (
 
 func (k msgServer) ChangeState(goCtx context.Context, msg *types.MsgChangeState) (*types.MsgChangeStateResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	if !k.IsAdminAccount(ctx, msg.Creator) {
+		return nil, types.ErrNotAdminAccount
+	}
 	projectId, err := k.ChangeProjectState(ctx, msg.NewState, msg.ProjectId)
 	if err != nil {
 		return &types.MsgChangeStateResponse{}, err
